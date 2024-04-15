c0f067f15ac0676436868dab6662b6e0a252052f
Sooraj Philip <soorajphilip97@gmail.com>
2024-04-15 18:26:48 +0530

    restrict signed in user from accessing home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+  before_action :redirect_signed_in_users_to_dashboard
 
   def index
+  end
+
+  private
+
+  def redirect_signed_in_users_to_dashboard
+    redirect_to tree_index_path if signed_in?
   end
 end
