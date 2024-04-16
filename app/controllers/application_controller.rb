d9571e2e132d2f6c2ce37939e25144a3a45e5a35
Sooraj Philip <soorajphilip97@gmail.com>
2024-04-16 16:44:22 +0530

    add name field to sign up page, add app logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_user
 
   def after_sign_in_path_for(resource)
     tree_index_path
+  end
+
+  private
+
+  def set_user
+    @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
   root "home#index"
   resources :tree, only: :index
   resources :links
+  resources :users
 end
